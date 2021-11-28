6b53e0e67278542073e105280156bab77c25fcdc
Red Penguin <79577742+redpenguinyt@users.noreply.github.com>
2021-11-28 21:57:51 +0000

    simplified library imports

--- cogs/events.py
+++ cogs/events.py
@@ -1,8 +1,8 @@
-import discord, os
 from datetime import datetime
 from discord.ext import commands
 from discord.ext.commands import errors
 from utils import config, traceback_maker, getinfofromguild, prettysend
+import discord
 
 class Events(commands.Cog):
 	def __init__(self, bot):
